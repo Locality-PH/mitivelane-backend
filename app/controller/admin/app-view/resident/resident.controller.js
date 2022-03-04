6df81f3fe97288b0524159fe724d321aacadd9aa
GiannPogi <gcmediavillo@gmail.com>
2022-03-04 08:47:14 +0800

    Resident done

--- app/controller/admin/app-view/resident/resident.controller.js
+++ app/controller/admin/app-view/resident/resident.controller.js
@@ -4,10 +4,10 @@ var mongoose = require("mongoose");
 const Resident = db.resident;
 
 exports.getResidents = async (req, res) => {
-    //console.log(req.body);
+    const barangay_id = req.body.barangay_id
 
     try {
-        const resident = await Resident.find({})
+        const resident = await Resident.find({ barangay_id })
         res.json(resident)
     } catch (error) {
         console.log(error)
@@ -15,16 +15,40 @@ exports.getResidents = async (req, res) => {
 };
 
 exports.addResident = async (req, res) => {
-    console.log(req.body);
     const newResidentData = req.body.values
     newResidentData._id = new mongoose.Types.ObjectId();
-    newResidentData.barangay_id = 1004;
+    newResidentData.barangay_id = mongoose.Types.ObjectId(req.body.barangay_id);
 
     try {
         const newResident = new Resident(newResidentData)
         console.log(newResident)
         await newResident.save()
         res.json("success")
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+exports.deleteResident = async (req, res) => {
+    const barangay_id = req.body.barangay_id
+    const resident_id = mongoose.Types.ObjectId(barangay_id);
+
+    try {
+        await Resident.findOneAndDelete({_id: resident_id})
+        res.json("deleted")
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+exports.updateResident = async (req, res) => {
+    const newResidentData = req.body.values
+    console.log("new val", newResidentData)
+    const resident_id = req.body.resident_id
+
+    try {
+        const query = await Resident.findByIdAndUpdate(resident_id, newResidentData)
+        res.json("updated")
     } catch (error) {
         console.log(error)
     }
