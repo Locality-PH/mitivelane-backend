21b8198490cb5a5e1dfb74f87bcdc51ecd0706e6
Rojhon <rojhonbuenaventura5@gmail.com>
2022-04-13 07:47:19 +0800

    Blotter

--- app/routes/admin/app-view/blotter/blotter_request.routes.js
+++ app/routes/admin/app-view/blotter/blotter_request.routes.js
@@ -5,7 +5,14 @@ module.exports = (app) => {
 
     router.post("/request-blotter", auth.authenticationToken, blotterRequest.requestBlotter);
     router.get("/get-blotter-request/:barangay_id", auth.authenticationToken, blotterRequest.getBlotterRequest);
+	
+	router.post("/approve-blotter-request", auth.authenticationToken, blotterRequest.approveBlotterRequest);
+	router.post("/reject-blotter-request", auth.authenticationToken, blotterRequest.rejectBlotterRequest);
+	
+	router.get("/get-pending-blotter-request/:barangay_id", auth.authenticationToken, blotterRequest.getPendingBlotterRequest);
 	router.post("/delete-blotter-request", auth.authenticationToken, blotterRequest.deleteBlotterRequest);
+	
+	router.get("/record-status/:barangay_id", auth.authenticationToken, blotterRequest.recordStatus);
 	
 	router.get("/get-latest-blotter-requests/:barangay_id", auth.authenticationToken, blotterRequest.getLatestBlotterRequests);
 
