5484b39dde9e490c43d142b750bccbcbcd0015fd
Rojhon <rojhonbuenaventura5@gmail.com>
2022-04-28 13:57:35 +0800

    test message

--- server.js
+++ server.js
@@ -47,6 +47,7 @@ const io = socketIO(server);
 
 io.on("connection", (socket) => {
   console.log("Client connected");
+  socket.to(user.socketId).emit("chat:receive-message", "ds", "ds")
   socket.on("disconnect", () => console.log("Client disconnected"));
 });
 const db = require("./app/models");
