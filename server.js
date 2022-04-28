92d5ca202164171c7a0bc78b81fff43a9f320d1e
Rojhon <rojhonbuenaventura5@gmail.com>
2022-04-28 15:05:40 +0800

    test message

--- server.js
+++ server.js
@@ -45,6 +45,8 @@ const io = socketIO(server);
 //   },
 // });
 
+io.set("transports", ["websocket", "polling", "flashsocket"])
+
 io.on("connection", (socket) => {
   console.log("Client connected");
   socket.on("disconnect", () => console.log("Client disconnected"));
