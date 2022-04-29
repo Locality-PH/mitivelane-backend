28a889f7fbca4cbc8a892628d9f5ebc060372409
Rojhon <rojhonbuenaventura5@gmail.com>
2022-04-29 11:31:48 +0800

    test message

--- app/socket/index.js
+++ app/socket/index.js
@@ -48,7 +48,21 @@ module.exports = (io) => {
 		  }, 10000)
 		
 		// Chat 
-		chat(socket, getUser)
+		
+		const sendMessage = (conversationId, receiverAuthToken, message) => {
+		// const user = getUser(receiverAuthToken)
+		console.log(message)
+		
+		try{
+			socket.to(socket.id).emit("chat:receive-message", conversationId, message)
+		}catch(error){
+			// Do nothing for now
+			
+		}
+	}
+	
+	socket.on("chat:send-message", sendMessage)
+		// chat(socket, getUser)
 
 	}
 
